6744f7b3b20e6e19f1c6061e98d35cc19779d306
shahsaahab-dev <naqishah2997@gmail.com>
2021-03-08 13:58:58 -0800

    changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 jQuery(document).ready(function($){
-    console.log("ready");
+    
+
+    // Opening the Side Drawer 
+    $(".account").on("click",function(e){
+        e.preventDefault();
+        $("#login-drawer").fadeIn(300);
+        $(".opacity-filter").fadeIn(300);
+    });
+
+    // Closing the Opacity Filter
+    $(".opacity-filter").on("click",function(e){
+        $(".side-drawer").fadeOut(300);
+        $(this).fadeOut(300);
+    });
+
+
+    $("#search").on("click",function(e){
+        $(".search-area").toggle(300);
+    })
 })
